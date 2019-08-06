32a26dad6a45edd5400456da36bd62f0fcfd6b80
Benjamin Kleeman <benjamin.kleeman@gmail.com>
2019-08-06 18:24:13 +0000

    energy-maps.init.plants.nff.v0.js edited online with Bitbucket

--- energy_maps/static/js/energy-maps.init.plants.nff.v0.js
+++ energy_maps/static/js/energy-maps.init.plants.nff.v0.js
@@ -12,22 +12,41 @@
   const ctx = canvas.node().getContext('2d');
   ctx.LineCap = 'round';
 
-  Promise.all([
-    d3.json(fmap),
-    d3.json(fmapfill)
-  ]).then(function(files) {
-    draw_land(ctx, files);
-  }).then(function() {
+  function draw_base_map() {
+    Promise.all([
+      d3.json(fmap),
+      d3.json(fmapfill)
+    ]).then(function(files) {
+      draw_land(ctx, files);
+    })
+  }
+
+  function draw_nff() {
     Promise.all([d3.json('/static/json/PowerPlants_US_2014Aug_R.geojson')])
-      .then(function(files) {
-        draw_nff_plants(ctx, files);
-      }).then(function() {
-        Promise.all([
-          d3.json(fmap),
-          d3.json(fmapfill)
-        ]).then(function(files) {
-          draw_land(ctx, files, true);
-        });
-      });
-  });
+    .then(function(files) {
+      draw_nff_plants(ctx, files);
+    })
+  }
+
+  draw_base_map();
+  draw_nff();
+
+  // Promise.all([
+  //   d3.json(fmap),
+  //   d3.json(fmapfill)
+  // ]).then(function(files) {
+  //   draw_land(ctx, files);
+  // }).then(function() {
+  //   Promise.all([d3.json('/static/json/PowerPlants_US_2014Aug_R.geojson')])
+  //     .then(function(files) {
+  //       draw_nff_plants(ctx, files);
+  //     }).then(function() {
+  //       Promise.all([
+  //         d3.json(fmap),
+  //         d3.json(fmapfill)
+  //       ]).then(function(files) {
+  //         draw_land(ctx, files, true);
+  //       });
+  //     });
+  // });
 })();
