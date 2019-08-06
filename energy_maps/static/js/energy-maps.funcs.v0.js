921ee7e35ea7a05706c81cfc7d0769fbb03c013e
Benjamin Kleeman <benjamin.kleeman@gmail.com>
2019-08-06 18:33:39 +0000

    energy-maps.funcs.v0.js edited online with Bitbucket

--- energy_maps/static/js/energy-maps.funcs.v0.js
+++ energy_maps/static/js/energy-maps.funcs.v0.js
@@ -1,17 +1,21 @@
-
+/**
+ * Draw the base map.
+ * @param {Object} ctx - HTML5 <canvas> element
+ * @param {Object[]} queued_data - Dataset for the corresponding resource
+ * @param {Object} border_only
+ */
 const draw_land = function draw_land(ctx, queued_data, border_only) {
-  console.log('draw_land');
+  console.log("draw_land");
 
   let map_data = queued_data[0];
   let map_fill_data = queued_data[1];
   path.context(ctx);
 
   if (!border_only) {
-
     // Sphere
     ctx.fillStyle = viz.map.ocean;
     ctx.beginPath();
-    path({type: 'Sphere'});
+    path({ type: "Sphere" });
     ctx.fill();
 
     // Land boundaries fill
@@ -19,55 +23,58 @@ const draw_land = function draw_land(ctx, queued_data, border_only) {
     ctx.beginPath();
     path(map_fill_data);
     ctx.fill();
-
   } else {
-
     ctx.strokeStyle = viz.map.stroke;
     ctx.lineWidth = viz.map.width;
     ctx.beginPath();
     path(map_data);
     ctx.stroke();
-
   }
-
 };
 
 /// Primitives
 
 const draw_x = function draw_x(ctx, xy, d) {
-  ctx.moveTo(xy[0]-d/2, xy[1]-d/2);
-  ctx.lineTo(xy[0]+d/2, xy[1]+d/2);
-  ctx.moveTo(xy[0]-d/2, xy[1]+d/2);
-  ctx.lineTo(xy[0]+d/2, xy[1]-d/2);
+  ctx.moveTo(xy[0] - d / 2, xy[1] - d / 2);
+  ctx.lineTo(xy[0] + d / 2, xy[1] + d / 2);
+  ctx.moveTo(xy[0] - d / 2, xy[1] + d / 2);
+  ctx.lineTo(xy[0] + d / 2, xy[1] - d / 2);
 };
 
 const draw_circle = function draw_circle(ctx, xy, r) {
-  ctx.arc(xy[0], xy[1], r, 0, Math.PI * 2, true)
+  ctx.arc(xy[0], xy[1], r, 0, Math.PI * 2, true);
 };
 
 const draw_box = function draw_box(ctx, xy, d) {
-  ctx.rect(xy[0]-d/2, xy[1]-d/2, d, d);
+  ctx.rect(xy[0] - d / 2, xy[1] - d / 2, d, d);
 };
 
 const draw_triangle = function draw_triangle(ctx, xy, d) {
-  let e = d*Math.sqrt(3)/3;
-  ctx.moveTo(xy[0], xy[1]-e*2);
-  ctx.lineTo(xy[0]+d, xy[1]+e);
-  ctx.lineTo(xy[0]-d, xy[1]+e);
-  ctx.lineTo(xy[0], xy[1]-e*2);
+  let e = (d * Math.sqrt(3)) / 3;
+  ctx.moveTo(xy[0], xy[1] - e * 2);
+  ctx.lineTo(xy[0] + d, xy[1] + e);
+  ctx.lineTo(xy[0] - d, xy[1] + e);
+  ctx.lineTo(xy[0], xy[1] - e * 2);
 };
 
 const draw_triangle_down = function draw_triangle_down(ctx, xy, d) {
-  let e = d*Math.sqrt(3)/3;
-  ctx.moveTo(xy[0], xy[1]+e*2);
-  ctx.lineTo(xy[0]+d, xy[1]-e);
-  ctx.lineTo(xy[0]-d, xy[1]-e);
-  ctx.lineTo(xy[0], xy[1]+e*2);
+  let e = (d * Math.sqrt(3)) / 3;
+  ctx.moveTo(xy[0], xy[1] + e * 2);
+  ctx.lineTo(xy[0] + d, xy[1] - e);
+  ctx.lineTo(xy[0] - d, xy[1] - e);
+  ctx.lineTo(xy[0], xy[1] + e * 2);
 };
 
 const draw_cross = function draw_cross(ctx, xy, d) {
-  ctx.moveTo(xy[0], xy[1]-d/2);
-  ctx.lineTo(xy[0], xy[1]+d/2);
-  ctx.moveTo(xy[0]-d/2, xy[1]);
-  ctx.lineTo(xy[0]+d/2, xy[1]);
+  ctx.moveTo(xy[0], xy[1] - d / 2);
+  ctx.lineTo(xy[0], xy[1] + d / 2);
+  ctx.moveTo(xy[0] - d / 2, xy[1]);
+  ctx.lineTo(xy[0] + d / 2, xy[1]);
 };
+
+// const toggle_layer = function toggle_layer(layer) {
+//   // Select all check boxes
+//   d3.selectAll(".checkbox");
+//   // if check box value equal to @param layer,
+//   // toggle layer on/off
+// };
