32da7f900886bdeb908ed29a6e8f89e9c3ab8b85
bkleeman <benjamin.kleeman@gmail.com>
2019-08-29 16:28:58 -0500

    move some builder functions from init.builder to .funcs.builder

--- energy_maps/static/js/energy-maps.funcs.builder.v0.js
+++ energy_maps/static/js/energy-maps.funcs.builder.v0.js
@@ -1,5 +1,5 @@
-// Since all the heavy lifting for everything is already being done in the .x.funcs files, What needs to actually go in here?
-
+/** Draw a layer to the screen. 
+ * Pass the data from a JSON file through a drawing function out to a canvas context. */
 function draw_json_layer(path, draw_function, context) {
     Promise.all([
       d3.json(path)
@@ -8,3 +8,37 @@ function draw_json_layer(path, draw_function, context) {
     })
     console.log(`draw json layer`);
   }  
+
+/** @description an array that contains the asset value of all active layers */
+let asset_total = [0];
+/** @description the total sum of asset values for all active layers */
+let asset_total_sum;
+
+/** Add total asset value of all active layers.
+   * Numeral.js is used for currency formatting (http://numeraljs.com/#format)
+   */
+  function calc_asset_total_sum() {
+    // use Array.reduce() as an accumulator to add up all values in the array
+    asset_total_sum = asset_total.reduce((total, amount) => total + amount);
+    document.getElementById(
+      "asset-totals"
+    // ).innerHTML = `<p>Asset totals: Approximately ${numeral(asset_total_sum).format('$0.000 a').toUpperCase()} of $10.5 T shown.</p>`;
+    ).innerHTML = `<p>Asset totals: Approximately ${numeral(asset_total_sum).format('$0.00a').toUpperCase()} of $10.5T shown.</p>`;
+  }
+
+  /** Add the passed value to the asset total array and compute the new value */
+  function increment_asset_total(value) {
+    asset_total.push(value);
+    calc_asset_total_sum();
+  }
+
+  /** Remove the passed value from the asset total array and compute the new value */
+  function decrement_asset_total(value) {
+    for (let i in asset_total) {
+      if (asset_total[i] == value) {
+        asset_total.splice(i, 1)
+        break
+      }
+    }
+    calc_asset_total_sum();
+  }
--- energy_maps/static/js/energy-maps.init.builder.v0.js
+++ energy_maps/static/js/energy-maps.init.builder.v0.js
@@ -150,42 +150,42 @@
     ff_val, // fossil fuel plant
     electrical_grid_val // electrical grid
   ];
-  /** @description an array containing the asset value of all active layers */
-  let asset_total = [0];
-  /** @description the total sum of asset values for all active layers */
-  let asset_total_sum;
+  // /** @description an array containing the asset value of all active layers */
+  // let asset_total = [0];
+  // /** @description the total sum of asset values for all active layers */
+  // let asset_total_sum;
 
-  /** Add total asset value of all active layers.
-   * Numeral.js is used for currency formatting (http://numeraljs.com/#format)
-   * TODO: Format precision based on value.
-   * Some are displaying an ugly format like "45.000 T." 
-   * Three trailing zeroes is not cute
-   */
-  function calc_asset_total_sum() {
-    // use Array.reduce() as an accumulator to add up all values in the array
-    asset_total_sum = asset_total.reduce((total, amount) => total + amount);
-    document.getElementById(
-      "asset-totals"
-    // ).innerHTML = `<p>Asset totals: Approximately ${numeral(asset_total_sum).format('$0.000 a').toUpperCase()} of $10.5 T shown.</p>`;
-    ).innerHTML = `<p>Asset totals: Approximately ${numeral(asset_total_sum).format('$0.00a').toUpperCase()} of $10.5T shown.</p>`;
-  }
+  // /** Add total asset value of all active layers.
+  //  * Numeral.js is used for currency formatting (http://numeraljs.com/#format)
+  //  * TODO: Format precision based on value.
+  //  * Some are displaying an ugly format like "45.000 T." 
+  //  * Three trailing zeroes is not cute
+  //  */
+  // function calc_asset_total_sum() {
+  //   // use Array.reduce() as an accumulator to add up all values in the array
+  //   asset_total_sum = asset_total.reduce((total, amount) => total + amount);
+  //   document.getElementById(
+  //     "asset-totals"
+  //   // ).innerHTML = `<p>Asset totals: Approximately ${numeral(asset_total_sum).format('$0.000 a').toUpperCase()} of $10.5 T shown.</p>`;
+  //   ).innerHTML = `<p>Asset totals: Approximately ${numeral(asset_total_sum).format('$0.00a').toUpperCase()} of $10.5T shown.</p>`;
+  // }
 
-  /** Add the passed value to the asset total array and compute the new value */
-  function increment_asset_total(value) {
-    asset_total.push(value);
-    calc_asset_total_sum();
-  }
+  // /** Add the passed value to the asset total array and compute the new value */
+  // function increment_asset_total(value) {
+  //   asset_total.push(value);
+  //   calc_asset_total_sum();
+  // }
 
-  /** Remove the passed value from the asset total array and compute the new value */
-  function decrement_asset_total(value) {
-    for (let i in asset_total) {
-      if (asset_total[i] == value) {
-        asset_total.splice(i, 1)
-        break
-      }
-    }
-    calc_asset_total_sum();
-  }
+  // /** Remove the passed value from the asset total array and compute the new value */
+  // function decrement_asset_total(value) {
+  //   for (let i in asset_total) {
+  //     if (asset_total[i] == value) {
+  //       asset_total.splice(i, 1)
+  //       break
+  //     }
+  //   }
+  //   calc_asset_total_sum();
+  // }
 
   /** @description An object of layers mapped to canvas nodes. Used to dynamically generate map layer divs and attach their canvases. */
   let canvases = {};
