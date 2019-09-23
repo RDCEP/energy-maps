d07c0cb981a16347b19a619c23ae18e76fc02b1d
Nathan Matteson <matteson@obstructures.org>
2019-09-23 17:56:29 -0500

    Coal mines are pentagons

--- energy_maps/static/js/energy-maps.funcs.coal.v0.js
+++ energy_maps/static/js/energy-maps.funcs.coal.v0.js
@@ -55,19 +55,28 @@ const draw_coal_mines = function draw_coal_mines(ctx, queued_data) {
 };
 
 /**
- * Helper function for draw_coal_mines to draw the circle representing the mine.
+ * Helper function for draw_coal_mines to draw the pentagon representing the mine.
  * @param {Object} ctx - HTML5 <canvas> element.
  * @param {Number} xy
  * @param {Object} color
  * @param {Number} r - Radius. 
  */
 const draw_mine = function draw_mine(ctx, xy, color, r) {
-
+  r = Math.sqrt(r / Math.PI) * viz.mines.scale;
   ctx.strokeStyle = viz.mines.coal.stroke;
   ctx.strokeWidth = viz.mines.coal.width;
   ctx.fillStyle = viz.mines.coal.fill;
+
   ctx.beginPath();
-  draw_circle(ctx, xy, Math.sqrt(r / Math.PI) * viz.mines.scale);
+
+  let a = Math.PI / 2;
+  let n = 5;
+  ctx.moveTo (xy[0] + r * Math.cos(a), xy[1] + r * Math.sin(a));
+  for (let i = 1; i <= n; ++i) {
+    ctx.lineTo (xy[0] + r * Math.cos(a + i * 2 * Math.PI / n),
+      xy[1] + r * Math.sin(a + i * 2 * Math.PI / n));
+  }
+
   ctx.fill();
   if (r > 500000) {
     ctx.stroke();
