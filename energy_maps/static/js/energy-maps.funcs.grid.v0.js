ae2613d6e1f0e004b165d936393645fa7045316c
bkleeman <benjamin.kleeman@gmail.com>
2020-01-08 16:06:46 -0600

    remove extraneous functions

--- energy_maps/static/js/energy-maps.funcs.grid.v0.js
+++ energy_maps/static/js/energy-maps.funcs.grid.v0.js
@@ -64,7 +64,7 @@ const line_width_kludge = function line_width_kludge(idx) {
  * Draw a grid class on the electric grid infrastructure map.
  * @param {Object} ctx
  * @param {array} queued_data
- * @param {Number} c -- index of the array of grid classes
+ * @param {Object} c -- object member of `classes`
  */
 draw_grid_class = function draw_grid_class(ctx, queued_data, c) {
   let grid = queued_data[0];
@@ -91,15 +91,14 @@ draw_grid_class = function draw_grid_class(ctx, queued_data, c) {
       ctx.strokeStyle = viz.grid.palette[c.value];
     }
 
-
     ctx.beginPath();
     path(tmp_grid);
     ctx.stroke();
+
       if (i === feat_len - 1) { 
         hide_spinner(); 
       }
     }
-  
 };
 
 /**
@@ -108,8 +107,8 @@ draw_grid_class = function draw_grid_class(ctx, queued_data, c) {
  * @param {Array} queued_data
  */
 const draw_grid_class_ac_unk_and_under_100 = function draw_grid_class_ac_unk_and_under_100(ctx, queued_data) {
-  draw_grid_class_unavailable(ctx, queued_data);
-  draw_grid_class_ac_under_100(ctx, queued_data);
+  draw_grid_class(ctx, queued_data, classes.GRID_CLASS_NA)
+  draw_grid_class(ctx, queued_data, classes.GRID_CLASS_UNDER_100)
 };
 
 /**
@@ -118,8 +117,8 @@ const draw_grid_class_ac_unk_and_under_100 = function draw_grid_class_ac_unk_and
  * @param {Array} queued_data
  */
 const draw_grid_class_ac_100_300 = function draw_grid_class_ac_100_300(ctx, queued_data) {
-  draw_grid_class_ac_100_200(ctx, queued_data);
-  draw_grid_class_ac_200_300(ctx, queued_data);
+  draw_grid_class(ctx, queued_data, classes.GRID_CLASS_100_200)
+  draw_grid_class(ctx, queued_data, classes.GRID_CLASS_200_300)
 };
 
 /**
@@ -128,84 +127,16 @@ const draw_grid_class_ac_100_300 = function draw_grid_class_ac_100_300(ctx, queu
  * @param {Array} queued_data
  */
 const draw_grid_class_ac_345_735 = function draw_grid_class_ac_345_735(ctx, queued_data) {
-  draw_grid_class_ac_345(ctx, queued_data);
-  // draw_grid_class(ctx, queued_data, classes.GRID_CLASS_345)
-  draw_grid_class_ac_500(ctx, queued_data);
-  // draw_grid_class(ctx, queued_data, classes.GRID_CLASS_500)
-  draw_grid_class_ac_735(ctx, queued_data);
-  // draw_grid_class(ctx, queued_data, classes.GRID_CLASS_735_PLUS)
-};
-
-/**
- * Draw grid class unavailable on the electric grid infrastructure map.
- * @param {Object} ctx
- * @param {Array} queued_data
- */
-const draw_grid_class_unavailable = function draw_grid_class_unavailable(ctx, queued_data) {
-  console.log('draw_grid_class_unavailable')
-  draw_grid_class(ctx, queued_data, classes.GRID_CLASS_NA)
-};
-
-/**
- * Draw grid class under 100 on the electric grid infrastructure map.
- * @param {Object} ctx
- * @param {Array} queued_data
- */
-const draw_grid_class_ac_under_100 = function draw_grid_class_ac_under_100(ctx, queued_data) {
-  console.log('electrical-grid-ac-lines-under-100-kv')
-  draw_grid_class(ctx, queued_data, classes.GRID_CLASS_UNDER_100)
-}  
-
-/**
- * Draw grid class 100-200 on the electric grid infrastructure map.
- * @param {Object} ctx
- * @param {Array} queued_data
- */
-const draw_grid_class_ac_100_200 = function draw_grid_class_ac_100_200(ctx, queued_data) {
-  console.log('electrical-grid-ac-lines-100-200')
-  draw_grid_class(ctx, queued_data, classes.GRID_CLASS_100_200)
-}
-
-/**
- * Draw grid class 200-300 on the electric grid infrastructure map.
- * @param {Object} ctx
- * @param {Array} queued_data
- */
-const draw_grid_class_ac_200_300 = function draw_grid_class_ac_200_300 (ctx, queued_data) {
-  console.log('electrical-grid-ac-lines-200-to-300-kv')
-  draw_grid_class(ctx, queued_data, classes.GRID_CLASS_200_300)
-}
-
-/**
- * Draw grid class 345 on the electric grid infrastructure map.
- * @param {Object} ctx
- * @param {Array} queued_data
- */
-const draw_grid_class_ac_345 = function draw_grid_class_ac_345 (ctx, queued_data) {
-  console.log('electrical-grid-ac-lines-345-kV')
   draw_grid_class(ctx, queued_data, classes.GRID_CLASS_345)
-}
-
-/**
- * Draw grid class 500 on the electric grid infrastructure map.
- * @param {Object} ctx
- * @param {Array} queued_data
- */
-const draw_grid_class_ac_500 = function draw_grid_class_ac_500 (ctx, queued_data) {
-  console.log('electrical-grid-ac-lines-500-kV')
   draw_grid_class(ctx, queued_data, classes.GRID_CLASS_500)
-}
+  draw_grid_class(ctx, queued_data, classes.GRID_CLASS_735_PLUS)
+};
 
 /**
- * Draw grid class 735 on the electric grid infrastructure map.
+ * Draw grid class DC on the electric grid infrastructure map.
  * @param {Object} ctx
  * @param {Array} queued_data
  */
-const draw_grid_class_ac_735 = function draw_grid_class_ac_735 (ctx, queued_data) {
-  console.log('electrical-grid-ac-lines-735+-kV')
-  draw_grid_class(ctx, queued_data, classes.GRID_CLASS_735_PLUS)
-}
-
 const draw_grid_class_dc = function draw_grid_class_dc (ctx, queued_data) {
   console.log('electrical-grid-dc-lines')
   draw_grid_class(ctx, queued_data, classes.GRID_CLASS_DC)
