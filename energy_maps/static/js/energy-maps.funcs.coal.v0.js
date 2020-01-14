af9bd2c468f522b73c368499df73a151ebdb1da9
bkleeman <benjamin.kleeman@gmail.com>
2020-01-14 14:25:26 -0600

    housekeeping

--- energy_maps/static/js/energy-maps.funcs.coal.v0.js
+++ energy_maps/static/js/energy-maps.funcs.coal.v0.js
@@ -13,24 +13,24 @@
  */
 
 /**
- * Draw railroads on the coal infrastructure map.
- * @param {Object} ctx - HTML5 canvas context..
- * @param {coal_mine[]} queued_data - Dataset for the corresponding resource
+ * Helper function for draw_coal_mines to draw the pentagon representing the mine.
+ * @param {Object} ctx - HTML5 canvas context
+ * @param {Array} xy - Array of xy coordinates 
+ * @param {Object} color
+ * @param {Number} r - Radius. 
  */
-const draw_railroads = function draw_railroads(ctx, queued_data) {
-  console.log('draw_railroads');
-
-  let railroad = queued_data[0];
-
-  const path = get_path(ctx);
-  // Gas pipeline
-  ctx.strokeStyle = viz.transport.rail.stroke;
-  ctx.lineWidth = viz.transport.rail.width;
+const draw_mine = function draw_mine(ctx, xy, color, r) {
+  const NUM_SIDES_MINE = 5;
+  r = Math.sqrt(r / Math.PI) * viz.mines.scale;
+  ctx.strokeStyle = viz.mines.coal.stroke;
+  ctx.strokeWidth = viz.mines.coal.width;
+  ctx.fillStyle = viz.mines.coal.fill;
   ctx.beginPath();
-  path(railroad);
-  ctx.stroke();
-  hide_spinner();
-
+  draw_polygon(NUM_SIDES_MINE, ctx, r, xy)
+  ctx.fill();
+  if (r > 8) {
+    ctx.stroke();
+  }
 };
 
 /**
@@ -40,7 +40,7 @@ const draw_railroads = function draw_railroads(ctx, queued_data) {
  */
 const draw_coal_mines = function draw_coal_mines(ctx, queued_data) {
   console.log('draw_coal_mines');
-
+  // TODO: why tf is this wells? Is this a duplicate of something related to the wells?
   let wells = queued_data[0];
 
   // Sort in descending order so large mines don't
@@ -63,22 +63,24 @@ const draw_coal_mines = function draw_coal_mines(ctx, queued_data) {
 };
 
 /**
- * Helper function for draw_coal_mines to draw the pentagon representing the mine.
- * @param {Object} ctx - HTML5 canvas context
- * @param {Array} xy - Array of xy coordinates 
- * @param {Object} color
- * @param {Number} r - Radius. 
+ * Draw railroads on the coal infrastructure map.
+ * @param {Object} ctx - HTML5 canvas context..
+ * @param {coal_mine[]} queued_data - Dataset for the corresponding resource
  */
-const draw_mine = function draw_mine(ctx, xy, color, r) {
-  const NUM_SIDES_MINE = 5;
-  r = Math.sqrt(r / Math.PI) * viz.mines.scale;
-  ctx.strokeStyle = viz.mines.coal.stroke;
-  ctx.strokeWidth = viz.mines.coal.width;
-  ctx.fillStyle = viz.mines.coal.fill;
+const draw_railroads = function draw_railroads(ctx, queued_data) {
+  console.log('draw_railroads');
+
+  let railroad = queued_data[0];
+
+  const path = get_path(ctx);
+  // Gas pipeline
+  ctx.strokeStyle = viz.transport.rail.stroke;
+  ctx.lineWidth = viz.transport.rail.width;
   ctx.beginPath();
-  draw_polygon(NUM_SIDES_MINE, ctx, r, xy)
-  ctx.fill();
-  if (r > 8) {
-    ctx.stroke();
-  }
+  path(railroad);
+  ctx.stroke();
+  hide_spinner();
+
 };
+
+
