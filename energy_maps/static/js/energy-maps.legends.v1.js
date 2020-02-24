8f6e9981ef8ba90718e4f560c2c3b86284c67062
bkleeman <benjamin.kleeman@gmail.com>
2020-02-24 17:13:04 -0600

    implement set_draw_legend() and draw_legend() for power plants to begin replacing the big ugly switch statement

--- energy_maps/static/js/energy-maps.legends.v1.js
+++ energy_maps/static/js/energy-maps.legends.v1.js
@@ -295,90 +295,104 @@ const update_legend = function update_legend(ctx, layers) {
   let y = 200 * SCALE;
   // Offset for text
   let text_offset = 30 * SCALE;
-    
+
+  let draw_legend = function draw_legend(ctx, x, y, obj) {
+    y = obj.draw_legend;
+    console.log(obj)
+    return y;
+  }
+
+  let set_draw_legend = function(ctx, x, y, obj) {
+    y = draw_power_plant_legend(ctx, x, y, obj)
+    console.log(y)
+    return y;
+  }
+
   // Consider using Function.prototype.bind() to bind ctx, x, and y to the object's properties (draw function with params for stroke and width)
   for (let i = 0; i < layers.length; ++i) {
     if (layers[i].active) { // TODO: consider this, if we can just pass an obj to the entire function ---> if (obj.active { // Do Stuff })
-      // y = layers[i].draw_legend(ctx, x, y, layers[i].obj);
       // y = draw_legend(ctx, x, y, layers[i]) // requires layers[i] to have a fucntion like draw_gas_storage(ctx, [x, y]); to be called off of it
-      // y = draw_well_legend(ctx, x, y, layers[i]);
-
-      switch (layers[i].name) {
-        case 'oil-well':
-          y = draw_well_legend(ctx, x, y, oil_well);
-          break;
-        case 'gas-well':
-          console.log('gas-well switch');
-          y = draw_well_legend(ctx, x, y, gas_well);
-          break;
-        case 'gas-pipeline':
-          y = draw_pipeline_legend(ctx, x, y, gas_pipeline);
-          break;
-        case 'oil-pipeline':          
-          y = draw_pipeline_legend(ctx, x, y, oil_pipeline);
-          break;
-        case 'oil-refinery':
-          y = draw_refinery_legend(ctx, x, y, oil_refinery);
-          break;
-        case 'railroad':;
-          y = draw_railroad_legend(ctx, x, y, railroad);
-          break;
-        case 'coal-mine':
-          y = draw_coalmine_legend(ctx, x, y, coal_mine);
-          break;
-        case 'coal-plant':
-          y = draw_power_plant_legend(ctx, x, y, coal_plants);
-          break;
-        case 'geothermal-plant':
-          y = draw_power_plant_legend(ctx, x, y, geo_plants);
-          break;
-        case 'biofuel':
-          y = draw_power_plant_legend(ctx, x, y, bio_plants);
-          break;
-        case 'hydro-plant':
-          y = draw_power_plant_legend(ctx, x, y, hyc_plants);
-          break;
-        case 'natural-gas-plant':
-          y = draw_power_plant_legend(ctx, x, y, ng_plants);
-          break;
-        case 'nuclear-plant':
-          y = draw_power_plant_legend(ctx, x, y, nuc_plants);
-          break;
-        case 'petroleum-plant':
-          y = draw_power_plant_legend(ctx, x, y, pet_plants);
-          break;
-        case 'solar-PV':
-          y = draw_power_plant_legend(ctx, x, y, solar_plants);
-          break;
-        case 'wind-farms':
-          y = draw_power_plant_legend(ctx, x, y, wnd_farms);
-          break;
-        case 'AC-lines-under-100-kV':
-          y = draw_grid_ac_legend(ctx, x, y, ac_na);
-          console.log(y)
-          y = draw_grid_ac_legend(ctx, x, y, ac_under_100);
-          break;
-        case 'AC-lines-100-to-300-kV':
-          y = draw_grid_ac_legend(ctx, x, y, ac_100_200);
-          y = draw_grid_ac_legend(ctx, x, y, ac_200_300);
-          break;
-        case 'AC-lines-345-to-735-kV':
-          y = draw_grid_ac_legend(ctx, x, y, ac_345);
-          y = draw_grid_ac_legend(ctx, x, y, ac_500);
-          y = draw_grid_ac_legend(ctx, x, y, ac_735_plus);
-          break;
-        case 'DC-lines':
-          y = draw_grid_dc_legend(ctx, x, y, dc);
-          break;
-        case 'gas-processing':
-          y = draw_processing_legend(ctx, x, y, gas_processing);
-          break;
-        case 'gas-storage':
-          y = draw_storage_legend(ctx, x, y, oil_and_gas.processing.gas_storage);
-          break;
-        default:
-          break;
-      }
+      // y = draw_legend(ctx, x, y, layers[i]); *** THIS ONE ***
+      layers[i].draw_legend = set_draw_legend(ctx, x, y, layers[i]);
+      y = layers[i].draw_legend;
+      y = draw_legend(ctx, x, y, layers[i])
+      // console.log(x)
+      // switch (layers[i].name) {
+      //   case 'oil-well':
+      //     y = draw_well_legend(ctx, x, y, oil_well);
+      //     break;
+      //   case 'gas-well':
+      //     console.log('gas-well switch');
+      //     y = draw_well_legend(ctx, x, y, gas_well);
+      //     break;
+      //   case 'gas-pipeline':
+      //     y = draw_pipeline_legend(ctx, x, y, gas_pipeline);
+      //     break;
+      //   case 'oil-pipeline':          
+      //     y = draw_pipeline_legend(ctx, x, y, oil_pipeline);
+      //     break;
+      //   case 'oil-refinery':
+      //     y = draw_refinery_legend(ctx, x, y, oil_refinery);
+      //     break;
+      //   case 'railroad':;
+      //     y = draw_railroad_legend(ctx, x, y, railroad);
+      //     break;
+      //   case 'coal-mine':
+      //     y = draw_coalmine_legend(ctx, x, y, coal_mine);
+      //     break;
+      //   case 'coal-plant':
+      //     y = draw_power_plant_legend(ctx, x, y, coal_plants);
+      //     break;
+      //   case 'geothermal-plant':
+      //     y = draw_power_plant_legend(ctx, x, y, geo_plants);
+      //     break;
+      //   case 'biofuel':
+      //     y = draw_power_plant_legend(ctx, x, y, bio_plants);
+      //     break;
+      //   case 'hydro-plant':
+      //     y = draw_power_plant_legend(ctx, x, y, hyc_plants);
+      //     break;
+      //   case 'natural-gas-plant':
+      //     y = draw_power_plant_legend(ctx, x, y, ng_plants);
+      //     break;
+      //   case 'nuclear-plant':
+      //     y = draw_power_plant_legend(ctx, x, y, nuc_plants);
+      //     break;
+      //   case 'petroleum-plant':
+      //     y = draw_power_plant_legend(ctx, x, y, pet_plants);
+      //     break;
+      //   case 'solar-PV':
+      //     y = draw_power_plant_legend(ctx, x, y, solar_plants);
+      //     break;
+      //   case 'wind-farms':
+      //     y = draw_power_plant_legend(ctx, x, y, wnd_farms);
+      //     break;
+      //   case 'AC-lines-under-100-kV':
+      //     y = draw_grid_ac_legend(ctx, x, y, ac_na);
+      //     console.log(y)
+      //     y = draw_grid_ac_legend(ctx, x, y, ac_under_100);
+      //     break;
+      //   case 'AC-lines-100-to-300-kV':
+      //     y = draw_grid_ac_legend(ctx, x, y, ac_100_200);
+      //     y = draw_grid_ac_legend(ctx, x, y, ac_200_300);
+      //     break;
+      //   case 'AC-lines-345-to-735-kV':
+      //     y = draw_grid_ac_legend(ctx, x, y, ac_345);
+      //     y = draw_grid_ac_legend(ctx, x, y, ac_500);
+      //     y = draw_grid_ac_legend(ctx, x, y, ac_735_plus);
+      //     break;
+      //   case 'DC-lines':
+      //     y = draw_grid_dc_legend(ctx, x, y, dc);
+      //     break;
+      //   case 'gas-processing':
+      //     y = draw_processing_legend(ctx, x, y, gas_processing);
+      //     break;
+      //   case 'gas-storage':
+      //     y = draw_storage_legend(ctx, x, y, oil_and_gas.processing.gas_storage);
+      //     break;
+      //   default:
+      //     break;
+      // }
     }
   }
 
