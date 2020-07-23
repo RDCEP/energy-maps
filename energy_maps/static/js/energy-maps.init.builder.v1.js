b0345061b173ed79408ce4ce5cfb25cf5d1d785c
Nathan Matteson <matteson@obstructures.org>
2020-07-23 16:05:30 -0500

    Comment out legend canvas instantiation

--- energy_maps/static/js/energy-maps.init.builder.v1.js
+++ energy_maps/static/js/energy-maps.init.builder.v1.js
@@ -58,19 +58,19 @@
    * @description HTML5 canvas for the application legend
    * @memberof Init
    */
-  let legend_canvas = d3
-    .select('.map.legend')
-    .append('canvas')
-    .attr('id', 'legendcanvas')
-    .attr('width', width)
-    .attr('height', height);
+  // let legend_canvas = d3
+  //   .select('.map.legend')
+  //   .append('canvas')
+  //   .attr('id', 'legendcanvas')
+  //   .attr('width', width)
+  //   .attr('height', height);
 
   /**
    * @type {Object}
    * @description HTML5 canvas context for the application legend
    * @memberof Init
    */  
-  let legend_ctx = legend_canvas.node().getContext('2d');
+  // let legend_ctx = legend_canvas.node().getContext('2d');
   ctx.LineCap = 'round';
 
   /**
