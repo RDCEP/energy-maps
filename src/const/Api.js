7aab5496f42fcc2a001832afadacd334394b7218
Nathan Matteson <matteson@obstructures.org>
2024-09-24 05:50:44 -0500

    Add deck to api string

--- src/const/Api.js
+++ src/const/Api.js
@@ -1,3 +1,3 @@
 
-export const api_url = 'https://energymapsapi.rdcep.dev/api/v0.1.0';
+export const api_url = 'https://energymapsapi.rdcep.dev/api/v0.1.0/deck';
 export const bbox = '-159,44;-132,-4;-101,5;-70,1;-48,51;-107,58;-159,44';
