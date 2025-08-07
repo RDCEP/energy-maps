f5932a8bd0a7501c39120fe49bd18dc5fa09fc9e
Nathan Matteson <matteson@obstructures.org>
2025-08-07 17:22:44 -0500

    Initial commit of drag handle

---
+++ src/components/left/DragHandle.js
@@ -0,0 +1,14 @@
+
+export const DragHandle = (props) => {
+
+  return (
+    <svg width="20" height="20" viewBox="0 0 20 20" aria-hidden="true">
+      <rect width="12" height="1" x="6" y="4"></rect>
+      <rect width="12" height="1" x="6" y="9"></rect>
+      <rect width="12" height="1" x="6" y="14"></rect>
+      <rect width="2" height="1" x="2" y="4"></rect>
+      <rect width="2" height="1" x="2" y="9"></rect>
+      <rect width="2" height="1" x="2" y="14"></rect>
+    </svg>
+  );
+};
