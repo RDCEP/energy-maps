d7f6f4842fe83fce279143fd0bfbd2afda203c64
Nathan Matteson <matteson@obstructures.org>
2025-08-19 17:18:44 -0500

    Add gas processing layer

--- src/components/map/layers/GasProcessing.js
+++ src/components/map/layers/GasProcessing.js
@@ -1,30 +1,42 @@
 import {GeoJsonLayer} from '@deck.gl/layers';
 import {api_url, bbox} from '../../../const/Api';
 import {iconAtlas, iconMapping} from '../MapIcons';
+import {extendLayer} from './extendLayer';
 
-export const GasProcessing = (zoomLevel) => {
+const ID = 'processing-gas';
+const LAYER_NAME = 'GasProcessing';
+const DISABLED = false;
 
-  return new GeoJsonLayer({
-    id: 'processing-gas',
-    layerName: 'GasProcessing',
-    componentName: 'GasProcessingLayer',
-    displayName: 'Gas Processing',
-    visible: false,
-    data: `${api_url}/processing_plants/gas/2012/1/1/${bbox}/`,
-    pickable: true,
-    autoHighlight: true,
+export const GasProcessing = extendLayer(
+  {
+    id: ID,
+    layerName: LAYER_NAME,
+    disabled: DISABLED,
+  },
+  function(zoomLevel, visible, assetValue) {
 
-    pointType: 'icon',
-    stroked: false,
-    filled: true,
-    iconAtlas: iconAtlas,
-    iconMapping: iconMapping,
-    getIcon: ()=> 'triangle',
-    getIconSize: function (d) {
-      return 12;
-    },
-    getIconColor: () => [0, 0, 139, 128],
-    iconSizeUnits: 'pixels',
-    iconSizeScale: 20 / Math.sqrt(zoomLevel),
+    return new GeoJsonLayer({
+      id: ID,
+      layerName: LAYER_NAME,
+      disabled: DISABLED,
+      componentName: `${LAYER_NAME}Layer`,
+      displayName: 'Gas Processing',
+      visible: visible,
+      data: `${api_url}/processing_plants/gas/2012/1/1/${bbox}/`,
+      pickable: true,
+      autoHighlight: true,
+
+      pointType: 'icon',
+      stroked: false,
+      filled: true,
+      iconAtlas: iconAtlas,
+      iconMapping: iconMapping,
+      getIcon: ()=> 'triangle',
+      getIconSize: function (d) {
+        return 20000;
+      },
+      getIconColor: () => [0, 0, 139, 128],
+      iconSizeUnits: 'meters',
+      iconSizeScale: 1 / (zoomLevel - 3),
+    });
   });
-};
